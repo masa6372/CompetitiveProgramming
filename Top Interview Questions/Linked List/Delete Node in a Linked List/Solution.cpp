e6130c998f8d837003f88d8addc0386012112587
masa6372 <songpuzheng@gmail.com>
2020-09-12 10:05:47 +0900

    Add a solution

--- Top Interview Questions/Linked List/Delete Node in a Linked List/Solution.cpp
+++ Top Interview Questions/Linked List/Delete Node in a Linked List/Solution.cpp
@@ -6,12 +6,13 @@ using namespace std;
 /*
  * Definition for singly-linked list.
  *
+*/
 struct ListNode {
      int val;
      ListNode *next;
      ListNode(int x) : val(x), next(NULL) {}
 };
-*/
+
 
 class Solution {
 public:
