73be7cbbaf236061177f9f83479cc26ce8f47018
masa6372 <songpuzheng@gmail.com>
2020-09-19 15:00:47 +0900

    Add a solution

---
+++ Top Interview Questions/Linked List/Reverse Linked List/Solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* res = head;
+        ListNode* ans = nullptr;
+        res->next = head;
+        while(res != nullptr) {
+            ListNode* nextTemp =  res -> next;
+            res -> next = ans;
+            ans = res;
+            res = nextTemp;
+        }        
+        return ans;
+    }
+};
